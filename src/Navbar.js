912de2862436ce3cc69194c658d5f3caeb715509
HieuHuynhDev30 <139535117+HieuHuynhDev30@users.noreply.github.com>
2023-07-15 21:24:40 +0700

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,10 @@ import { Outlet, Link, useNavigate } from "react-router-dom";
 import logo from "./images/movies-browser-low-resolution-logo-color-on-transparent-background.png";
 import Footer from "./Footer";
 import "./App.css";
+import React, { useState } from "react";
 
 function Navbar({ searchText, setSearchText }) {
+  const [isOpen, setIsOpen] = useState(true);
   const navigate = useNavigate();
   function updateSearch(event) {
     navigate("/moviecard");
@@ -14,30 +16,29 @@ function Navbar({ searchText, setSearchText }) {
     event.preventDefault();
     alert(`Showing results for ${searchText}`);
   }
+  if (isOpen) {
+    var collapse = 'open'
+  } else {
+    var collapse = 'close' 
+  }
+  const collapseClass = `${collapse} navbar-collapse drop`
   return (
     <>
       <div className="page-wrap text-light pb-5">
         <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
-          <div className="container-fluid">
+          <div className="container-fluid ">
             <Link className="navbar-brand" to="/">
               <img style={{ width: "15rem" }} src={logo} alt="logo" />
             </Link>
             <button
               className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+              onClick={() => setIsOpen(!isOpen)}
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <div className={collapseClass}>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                 <li className="nav-item">
                   <Link className="nav-link active" aria-current="page" to="/">
                     Home
